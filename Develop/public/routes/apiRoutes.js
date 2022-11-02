a01a5d7f80f4c8e29f402a42b3d93e8ae8f54897
Jules Joseph <julesj93@gmail.com>
2022-11-01 23:51:42 -0400

    added id to const require

--- Develop/public/routes/apiRoutes.js
+++ Develop/public/routes/apiRoutes.js
@@ -2,7 +2,7 @@ const Router = require("express").Router();
 const fs = require('fs');
 const util = require('util');
 // Helper method for generating unique ids
-const uuid = require('../helpers/uuid');
+const uuid = require("../../db.json");
 
 
 Router.get('/tips', (req, res) => {
