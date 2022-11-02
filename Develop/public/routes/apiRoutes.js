7cc5e4bf8bcb5ffb8fbc6f0eb19f246c60b22367
Jules Joseph <julesj93@gmail.com>
2022-11-01 23:49:03 -0400

    added structure for apiroutes

--- Develop/public/routes/apiRoutes.js
+++ Develop/public/routes/apiRoutes.js
@@ -3,3 +3,30 @@ const fs = require('fs');
 const util = require('util');
 // Helper method for generating unique ids
 const uuid = require('../helpers/uuid');
+
+
+Router.get('/tips', (req, res) => {
+    console.info(`${req.method} request received for tips`);
+    readFromFile('./db/tips.json').then((data) => res.json(JSON.parse(data)));
+  });
+  
+  // POST Route for a new UX/UI tip
+  Router.post('/tips', (req, res) => {
+    console.info(`${req.method} request received to add a tip`);
+  
+    const { username, topic, tip } = req.body;
+  
+    if (req.body) {
+      const newTip = {
+        username,
+        tip,
+        topic,
+        tip_id: uuid(),
+      };
+  
+      readAndAppend(newTip, './db/tips.json');
+      res.json(`Tip added successfully 🚀`);
+    } else {
+      res.error('Error in adding tip');
+    }
+  });
