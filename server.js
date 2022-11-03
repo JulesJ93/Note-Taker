0dfa6866bbdccacbca31c3e13cb0fe999d068f3c
Jules Joseph <julesj93@gmail.com>
2022-11-03 15:17:17 -0400

    setting up server.js

---
+++ server.js
@@ -0,0 +1,50 @@
+//const that grab the files we created in routes folder
+const apiRoutes = require('./routes/apiRoutes/api');
+const htmlRoutes = require('./routes/htmlRoutes/html');
+const express = require('express')
+//the port we will be using
+const PORT = 3001;
+
+const app = express();
+//making static public so css and html can access eachother
+app.use(express.static('public'));
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+//app.use is opening files that redirect apis as well as html which is defined in index.js
+app.use('/api', apiRoutes);
+app.use('/', htmlRoutes);
+
+app.listen(PORT, () =>
+  console.log(`App listening at http://localhost:${PORT} 🚀`)
+);
+
+const express = require('express');
+const path = require('path');
+const { clog } = require('./middleware/clog');
+const api = require('./routes/index.js');
+
+const PORT = 3001;
+
+const app = express();
+
+// Middleware for parsing JSON and urlencoded form data
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use('/api', api);
+
+app.use(express.static('public'));
+
+// GET Route for homepage
+app.get('/', (req, res) =>
+  res.sendFile(path.join(__dirname, '/public/index.html'))
+);
+
+// GET Route for feedback page
+app.get('/feedback', (req, res) =>
+  res.sendFile(path.join(__dirname, '/public/pages/feedback.html'))
+);
+
+app.listen(PORT, () =>
+  console.log(`App listening at http://localhost:${PORT} 🚀`)
+);
