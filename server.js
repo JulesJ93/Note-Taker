082fa39ec4df2b3a4d39ad9137a5bef70f7e8a5a
Jules Joseph <julesj93@gmail.com>
2022-11-03 15:24:19 -0400

    added get route for notes page

--- server.js
+++ server.js
@@ -42,7 +42,7 @@ app.get('/', (req, res) =>
   res.sendFile(path.join(__dirname, '/public/index.html'))
 );
 
-// GET Route for feedback page
+// GET Route for notes page
 app.get('/feedback', (req, res) =>
   res.sendFile(path.join(__dirname, '/public/notes.html'))
 );
