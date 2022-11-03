17dae3f6785bc83a7cabb66127eb85cfad339a81
Jules Joseph <julesj93@gmail.com>
2022-11-03 15:37:13 -0400

    added .. to route source

--- routes/htmlroutes.js
+++ routes/htmlroutes.js
@@ -4,7 +4,7 @@ const path = require('path');
 // GET Route for homepage
 module.exports = (router) => {
 Router.get("/notes", (req, res) =>
-  res.sendFile(path.join(__dirname, '/public/notes.html'))
+  res.sendFile(path.join(__dirname, '../public/notes.html'))
 );
 
 // GET Route for index.html page
